b8cb56f0be5257926e7e3183d79c34c14a4a9ada
Thabiso Makolana <makolanathabiso@gmail.com>
2020-05-05 11:36:28 +0200

    first commit

---
+++ animals.js
@@ -0,0 +1,43 @@
+class Animal {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+class Dog extends animal {
+    constructor() {
+        
+    }
+
+    sound() {
+        console.log(`${this.name} barks`)
+    }
+}
+
+class Cat extends animal {
+    constructor() {
+        
+    }
+
+    sound() {
+        console.log(`${this.name} meows`)
+    }
+}
+
+class Home {
+    #animals = []
+    constructor(animal){
+        this.animal = animal
+    }
+
+    adoptPet(animal){
+        this.#animals.push[animal]
+    }
+
+    makeAllSounds(){
+        this.#animals.forEach((animal , index)=>{
+            console.log(animal.sound)
+        })
+
+    }
+}
