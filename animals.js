e332cde3dc9bd8fbdba4ad3638e3fe127f6bde00
Thabiso Makolana <makolanathabiso@gmail.com>
2020-05-07 10:49:37 +0200

    touch ups

--- animals.js
+++ animals.js
@@ -3,45 +3,70 @@ class Animal {
         this.name = name
     }
 
-    eat(){
+    eat() {
         console.log(`${this.name} eats`)
     }
 }
 
 class Dog extends Animal {
-    constructor() {
-        
+    constructor(name) {
+        super(name)
     }
 
-    sound() {
+    sounds() {
         console.log(`${this.name} barks`)
     }
 }
 
 class Cat extends Animal {
-    constructor() {
-        
+    constructor(name) {
+        super(name)
     }
 
-    sound() {
+    sounds() {
         console.log(`${this.name} meows`)
     }
 }
 
 class Home {
     #animals = []
-    constructor(Animal){
-        this.Animal = Animal
+    constructor() {
+
     }
 
-    adoptPet(Animal){
-        this.#animals.push[Animal]
+    adoptPet(Animal) {
+        this.#animals.push(Animal)
     }
 
-    makeAllSounds(){
-        this.#animals.forEach((Animal , index)=>{
-            console.log(Animal.sound)
+    makeAllSounds() {
+        this.#animals.forEach((Animal, index) => {
+            console.log(Animal.sounds())
         })
 
     }
 }
+
+var dog = new Dog("Snoopy")
+
+dog.eat()
+dog.sounds()
+
+var cat = new Cat("Catty Purry")
+
+cat.eat()
+cat.sounds()
+
+var home = new Home()
+var dog1 = new Dog("Bobby")
+var dog2 = new Dog("Rex")
+var cat1 = new Cat("Doja cat")
+
+home.makeAllSounds() //outputs nothing
+home.adoptPet(dog1)
+home.makeAllSounds()
+
+home.adoptPet(cat1)
+home.makeAllSounds()
+
+home.adoptPet(dog2)
+home.makeAllSounds()
